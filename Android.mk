582c6fa930cf6d74a089c77cdc359dd3fe49ae91
Nyks45 <winglow45@gmail.com>
2016-11-13 23:00:36 +0300

    Fury Of A Dragon

--- Android.mk
+++ Android.mk
@@ -21,6 +21,6 @@ LOCAL_SRC_FILES := $(call all-subdir-java-files)
 
 LOCAL_MODULE_TAGS := optional
 
-LOCAL_PACKAGE_NAME := TytanWallpapers
+LOCAL_PACKAGE_NAME := FuryDragonsWallpapers
 
 include $(BUILD_PACKAGE)
